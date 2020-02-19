5991b8066849b3025a8496a78136afaa11aca702
Cam Nugent <nugentc@uoguelph.ca>
2020-02-19 11:59:58 -0500

    pseudocode for the alfie workflow

--- alfie/alf.py
+++ alfie/alf.py
@@ -1,8 +1,11 @@
 import argparse
 
+import seqio
 
 """
 command line executable script for processing data with alfie
+
+TODO - build the fasta single batch process, then adapt and repeat for the other 3 situations.
 
 """
 
@@ -16,13 +19,43 @@ def main():
 		"Input can be either fasta or fastq formatfile type inferred from the extension.\n"+\
 		"fasta: '.fasta' or '.fa'\n"+\
 		"fastq: '.fastq' or '.fq'\n")
-
-
+	parser.add_argument("-b", "--batch", action = "store_true", 
+		help = "should the input file be processed in batches of 1000 sequences?"+\
+		"Default is False, passing this flag will enable sub batches and decrease"+\
+		"the amount of data stored in ram at one time. tradeoff is slower ")
 
 	args = parser.parse_args()
 
+	#check if fasta or fastq input
+	ftype = seqio.file_type(args.file)
 
-	#check if fasta or fq
+	if ftype == 'fasta': 
+		if args.batch == True:
+			# batch fasta processing
 
 
-	#
+		else:
+			# full file fasta processing
+
+			# read in the file, store entries as tuples 
+
+			# once read in generate the kmer data
+
+			# turn kmer data into numpy array of proper structure (bind 3mer and 6mer, 
+			# and bind rows to make shape) 
+
+			# load the model
+
+			# pass the array of kmer data entries to the model
+
+			# use the predictions to write the entries to the correct file
+			# need the numeric encoding of the kingdoms stored here.
+
+
+
+	elif ftype == 'fastq':
+		if args.batch == True:
+			# batch fastq processing
+
+		else:
+			# full file fastq processing
