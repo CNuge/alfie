00b1009ebac634fac02973ea4af5959a064e936a
Cam Nugent <nugentc@uoguelph.ca>
2020-03-04 10:38:35 -0500

    unittest redesign for travis.yml
    no relative imports, use the data included in the package

--- alfie/test_seqio.py
+++ alfie/test_seqio.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 from seqio import file_type, outfile_dict, read_fasta, read_fastq
-
+from alfie import ex_fasta_file, ex_fastq_file
 
 class SeqioTests(unittest.TestCase):
 	"""
@@ -20,8 +20,8 @@ class SeqioTests(unittest.TestCase):
 										 3: 'alfie_out/plantae_test.fasta',
 										 4: 'alfie_out/protista_test.fasta'}
 		
-		self._fasta_infile = 'data/small_unittest.fasta'
-		self._fastq_infile = 'data/small_unittest.fastq'
+		self._fasta_infile = ex_fasta_file
+		self._fastq_infile = ex_fastq_file
 
 	@classmethod
 	def tearDown(self):
@@ -71,37 +71,37 @@ class SeqioTests(unittest.TestCase):
 	def test_fasta_reader(self):
 		self._fasta_read = read_fasta(self._fasta_infile)
 		
-		self.assertEqual(len(self._fasta_read), 3)
+		self.assertEqual(len(self._fasta_read), 100)
 		
 		self.assertEqual(self._fasta_read[0]['name'], 
-						"example_read_1")
+						"seq1_plantae")
 		self.assertEqual(self._fasta_read[1]['name'], 
-						"example_read_2")
+						"seq2_bacteria")
 		self.assertEqual(self._fasta_read[2]['name'], 
-						"example_read_3")
+						"seq3_protista")
 
 		self.assertEqual(self._fasta_read[0]['sequence'][:25], 
-						"gttcaacaaatcataaagatattgg")
+						"TTCTAGGAGCATGTATATCTATGCT")
 		self.assertEqual(self._fasta_read[1]['sequence'][:25],
-						"attcaaccaatcataaagatattgg")
+						"ACGGGCTTATCATGGTATTTGGTGC")
 		self.assertEqual(self._fasta_read[2]['sequence'][:25],
-						"gatatagcatttccacgacttaata")
+						"AGTATTAATTCGTATGGAATTAGCA")
 
 	def test_fastq_reader(self):
 		self._fastq_read = read_fastq(self._fastq_infile)
 
-		self.assertEqual(len(self._fastq_read), 3)
+		self.assertEqual(len(self._fastq_read), 100)
 	
 		for i in range(len(self._fastq_read)):
 			self.assertEqual(list(self._fastq_read[i].keys()),
 							['name', 'sequence', 'strand', 'quality'])
 
 		self.assertEqual(self._fastq_read[0]['sequence'][:25], 
-						"gttcaacaaatcataaagatattgg")
+						"ttctaggagcatgtatatctatgct")
 		self.assertEqual(self._fastq_read[1]['sequence'][:25],
-						"attcaaccaatcataaagatattgg")
+						"acgggcttatcatggtatttggtgc")
 		self.assertEqual(self._fastq_read[2]['sequence'][:25],
-						"cggaattcccggatcaataattggg")
+						"agtattaattcgtatggaattagca")
 
 
 if __name__ == '__main__':
