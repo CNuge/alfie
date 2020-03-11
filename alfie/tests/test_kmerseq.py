a3783457565116c3e54b675fb5d791045e2cebe5
Cam Nugent <nugentc@uoguelph.ca>
2020-03-11 10:55:12 -0400

    adding kmer tests for missing lines

--- alfie/tests/test_kmerseq.py
+++ alfie/tests/test_kmerseq.py
@@ -10,14 +10,28 @@ def test_KmerFeatures():
 
 	assert test_kmers.labels.shape == (256,)
 
-	assert list(test_kmers.labels[:3]) == ['AAAA', 'AAAC', 'AAAG']
+	assert list(test_kmers.labels[:3]) == ["AAAA", "AAAC", "AAAG"]
 
-	assert list(test_kmers.labels[-3:]) == ['TTTC', 'TTTG', 'TTTT']
+	assert list(test_kmers.labels[-3:]) == ["TTTC", "TTTG", "TTTT"]
 
 	assert test_kmers.kmer_freqs.shape == (256,)
+
+	assert test_kmers.items()[0] == ("AAAA", 3)
 
 	with pytest.raises(ValueError):
 		KmerFeatures("test1", "NOTDNA")
 
 
+	#chage the k, asser it was done properly
+	test_kmers.change_k(2)
 
+	assert test_kmers.kmer_freqs.shape == (16,)
+	assert list(test_kmers.labels[:4]) == ["AA", "AC", "AG", "AT"]
+	assert list(test_kmers.labels[-4:]) == ["TA", "TC", "TG", "TT"]
+
+
+	#test handling of empty kmer counts
+	#should make all the frequencies 0, while avoiding a divide by zero.
+	e_test = KmerFeatures("test2", "")
+
+	assert list(e_test.freq_values()) == [0]*256
